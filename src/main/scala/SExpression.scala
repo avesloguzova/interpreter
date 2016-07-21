3ca84edfeb70d9bca78755d38e0c4791e2ac0381
Alexandra Vesloguzova <sashickk@gmail.com>
2016-07-21 18:21:46 +0300

    Implement toString method for S-expression

--- src/main/scala/SExpression.scala
+++ src/main/scala/SExpression.scala
@@ -1,9 +1,16 @@
 sealed trait SExpression
 
-case class Number(value: Int) extends SExpression
+case class Number(value: Int) extends SExpression {
+  override def toString: String = value.toString
+}
 
-case class Atom(value: String) extends SExpression
+case class Atom(value: String) extends SExpression {
+  override def toString: String = value
+}
 
-case class List(value: Seq[SExpression]) extends SExpression
+case class List(value: Seq[SExpression]) extends SExpression {
+  override def toString: String = value.mkString("(", ",", ")")
+}
+
 
 
